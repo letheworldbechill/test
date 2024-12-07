5f7771548cdc5e2fb131ca8c820cb6a36cd0a636
letheworldbechill <gasserseverin1@gmail.com>
2024-12-07 15:49:53 +0100

    Add files via upload

---
+++ root/script.js
@@ -0,0 +1,4 @@
+document.getElementById('testButton').addEventListener('click', function() {
+    const output = document.getElementById('output');
+    output.textContent = 'Du hast den Button geklickt! GitHub Pages funktioniert.';
+});
