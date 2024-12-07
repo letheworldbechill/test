d15cad89bd3d83975e64d0e8de2aad9a5aef3394
letheworldbechill <gasserseverin1@gmail.com>
2024-12-07 16:02:33 +0100

    Delete root directory

--- root/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('testButton').addEventListener('click', function() {
-    const output = document.getElementById('output');
-    output.textContent = 'Du hast den Button geklickt! GitHub Pages funktioniert.';
-});
