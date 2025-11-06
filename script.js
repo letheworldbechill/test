96dde6d287d2d987c17f39e200d57dc831f75cce
Severin Leander <gasserseverin1@gmail.com>
2025-11-06 10:22:25 +0100

    Add files via upload
    Signed-off-by: Severin Leander <gasserseverin1@gmail.com>

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-document.getElementById('testButton').addEventListener('click', function() {
-    const output = document.getElementById('output');
-    output.textContent = 'Du hast den Button geklickt! GitHub Pages funktioniert.';
-});
+
+function calculateScore() {
+  let boxes = document.querySelectorAll('input[type=checkbox]');
+  let score = 0;
+  boxes.forEach(b=>{ if(b.checked) score += parseInt(b.value);});
+  let r = document.getElementById("result");
+  if(score < 4) r.innerText = "✅ Gesund -> Score: " + score;
+  else if(score < 6) r.innerText = "⚠️ Warnzeichen -> Score: " + score;
+  else r.innerText = "🚨 Hoch toxisch -> Score: " + score;
+}
